05e8f7ae2a9972de2dbbb56ced515762660e9b15
coltongraygg <coltongraygg@gmail.com>
2025-02-13 23:57:50 +0000

    Cleaned up imports and fixed db column types

--- app.py
+++ app.py
@@ -1,31 +1,30 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orgm import mapped_column
+from sqlalchemy import create_engine, DateTime, Integer, String, Float, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from fastapi import FastAPI
-from sqlalchemy import Date
+from datetime import datetime
+
 # define database url with database (sqlite), driver (pysqlite), and then path to save db file
-DATABASE_URL = 'sqlite+pysqlite:///home/colton/projects/ping_monitor/network_data.db'
+DATABASE_URL = 'sqlite+pysqlite:///home/colton/projects/ping_monitor/pings.db'
 
 
 
 
-class Base(DeclaritiveBase):
+class Base(DeclarativeBase):
   pass
 
-class Database(Base):
+class Pings(Base):
   __tablename__ = 'ping_tests'
   id: Mapped[int] = mapped_column(primary_key=True)
-  ping_address: Mapped[str] 
-  hop: Mapped[int]
-  hop_ip_address: Mapped[str]
-  hop_hostname: Mapped[str]
-  packet_loss: Mapped[float]
-  location: Mapped[str]
-  date_time: Mapped[Date]
+  ping_address: Mapped[str]  = mapped_column(String)
+  hop: Mapped[int] = mapped_column(Integer)
+  hop_ip_address: Mapped[str] = mapped_column(String)
+  hop_hostname: Mapped[str] = mapped_column(String)
+  packet_loss: Mapped[float] = mapped_column(Float)
+  location: Mapped[str] = mapped_column(String)
+  time_created: Mapped[datetime] = mapped_column(DateTime, default=func.now())
 
 
 
 # establish connection to db via sqlalchemy create_engine
 engine = create_engine(DATABASE_URL, echo=True)
-Base.metadata.create_all(engine)
+Base.metadata.create_all(engine) # execute
